fe0415831d72c63b155a908498f472c4e4970cae
Fernando Cortez <fccortez1@hotmail.com>
2014-02-25 22:03:20 -0500

    Renamed some files. Added SQL file where queries will be stored w/ instructions on who to output to file

---
+++ project/queries.sql
@@ -0,0 +1,10 @@
+-- Fernando Cortez
+-- CSC 4710
+-- Database Project
+-- 25 March 2014
+
+-- How to read queries from file and export to text file
+-- sqlite3 -column -header -echo project.db < queries.sql > output.txt
+-- The resulting file will contain the queries that were executed, and
+-- following each query, the result of such query formatted so that
+-- the table column headings appear with each line a resulting row
--- project/schema.sql
+++ project/schema.sql
@@ -3,6 +3,9 @@
 -- Database Project
 -- 25 March 2014
 
+-- How to export schema to database
+-- sqlite3 project.db < project_schema.sql
+
 CREATE TABLE tbl_customer (
     customer_id   INTEGER   PRIMARY KEY   AUTOINCREMENT,
     first_name    TEXT      NOT NULL,
