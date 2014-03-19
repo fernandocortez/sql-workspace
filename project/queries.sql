80d78b1fc6e6bca285c2337e559fd97b7e092074
Fernando Cortez <fccortez1@hotmail.com>
2014-03-19 17:44:29 -0400

    Wrote a few more queries

--- project/queries.sql
+++ project/queries.sql
@@ -10,12 +10,13 @@ SELECT customer_id FROM tbl_customer_vehicle GROUP BY customer_id HAVING count(v
 SELECT order_id, count(service_id) AS service_count FROM tbl_order_service GROUP BY order_id;
 
 -- List the first name and the last name of customers who own more than 2 vehicles
-SELECT first_name, last_name FROM tbl_customer NATURAL JOIN (SELECT customer_id, count(vehicle_id) AS vehicle_count FROM tbl_customer_vehicle GROUP BY customer_id HAVING vehicle_count > 2);
+SELECT first_name, last_name FROM tbl_customer JOIN (SELECT customer_id, count(vehicle_id) AS vehicle_count FROM tbl_customer_vehicle GROUP BY customer_id HAVING vehicle_count > 2) USING (customer_id);
 
 -- List the order id and cost of orders whose cost are more than 550, and the year, make, model and mileage of vehicles for which the order is placed on. Sort the result in ascending order by order id
-
+SELECT order_id, sum((SELECT labor_hour*labor_cost_per_hour+part_cost)) AS total_cost, year, make, model, mileage FROM tbl_order_service NATURAL JOIN tbl_service NATURAL JOIN tbl_rate NATURAL JOIN tbl_vehicle_order NATURAL JOIN tbl_vehicle GROUP BY order_id HAVING total_cost > 550 ORDER BY order_id;
 
 -- List the service id, name and cost of services whose costs are more than 500
+SELECT service_id, name, labor_hour*labor_cost_per_hour+part_cost AS service_cost FROM tbl_service JOIN tbl_rate USING (rate_id) WHERE service_cost > 500;
 
 -- List the customer id, first name, last name and total expense of customers. Sort the result is ascending order by customer last name, and then customer first name
 
